fe46a07886043b4a0548910eef38575f655e8e64
nisted <clement.p10@laposte.net>
2018-10-09 11:04:20 +0200

    premier depot

---
+++ script.js
@@ -0,0 +1,7 @@
+function mouse(){
+  document.getElementById('image1').src="/home/peltier/javascript/JavaScript_Partie2/Ex_5/assets/image1_2.jpg";
+  document.getElementById('image2').src="/home/peltier/javascript/JavaScript_Partie2/Ex_5/assets/image2_2.jpg";
+  document.getElementById('image3').src="/home/peltier/javascript/JavaScript_Partie2/Ex_5/assets/image3_2.jpg";
+  document.getElementById('image4').src="/home/peltier/javascript/JavaScript_Partie2/Ex_5/assets/image4_2.jpg"
+  document.getElementById('image5').src="/home/peltier/javascript/JavaScript_Partie2/Ex_5/assets/image5_2.jpg"
+}
